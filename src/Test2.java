9d099355774b6b8c1662269f67b2e16b7cc32bc4
Maithu8 <maithiliexpirment@gmail.com>
2023-07-31 18:53:38 +0530

    Commiting Test2 to Branch1

---
+++ src/Test2.java
@@ -0,0 +1,22 @@
+
+public class Test2 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		String hunger="yes";
+		
+		if(hunger.equals("yes"))
+		{
+			System.out.println("Eat Something.....");
+		}
+		else
+		{
+			System.out.println("Do Work..");
+		}
+		
+	}
+
+
+}
