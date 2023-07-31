e64daaa644783fdd5cc78d8cc0595c99a4815989
PandeMD <mathilipande65@gmail.com>
2023-07-31 17:55:36 +0530

    First....

---
+++ src/Test1.java
@@ -0,0 +1,21 @@
+
+public class Test1 
+{
+	
+public static void main(String[] args) {
+		
+		String hunger="yes";
+		
+		if(hunger.equals("yes"))
+		{
+			System.out.println("Eat Something.....");
+		}
+		else
+		{
+			System.out.println("Do Work..");
+		}
+		
+	}
+
+
+}
